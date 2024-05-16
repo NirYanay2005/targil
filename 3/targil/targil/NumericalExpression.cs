ea228bdab0269528ff6be91ca4db6a0ab45a2e57
Nir Yanay <niryanay2005@gmail.com>
2024-05-16 10:36:52 +0300

    Finished part 3 not inclding the bonus

--- 3/targil/targil/NumericalExpression.cs
+++ 3/targil/targil/NumericalExpression.cs
@@ -7,13 +7,31 @@ namespace targil
     class NumericalExpression
     {
         
-        private double MainNumber;
+        private double mainNumber;
         private static string[] factor = { "", " thousands,", " millions,", " billions," };
         private static string[] singles = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
         private static string[] tensWithOnes = { "", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
         private static string[] tens = { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-        public NumericalExpression(long pNumber) => MainNumber = pNumber;
 
+        public NumericalExpression(double pNumber) => mainNumber = pNumber;
+        public override string ToString()
+        {
+            double number = mainNumber;
+            int factorCounter = 0;
+            StringBuilder numInWords = new StringBuilder();
+
+            while (number != 0)
+            {
+                if (number % 1000 != 0)
+                {
+                    numInWords.Insert(0, readNumber(Convert.ToInt32(number % 1000)) + factor[factorCounter] + " ");
+                }
+                number -= number % 1000;
+                number /= 1000;
+                factorCounter++;
+            }
+            return numInWords.ToString();
+        }
         private string readNumber(int number)
         {
             StringBuilder numInWords = new StringBuilder();
@@ -38,25 +56,32 @@ namespace targil
             }
             return numInWords.ToString();
         }
+        public double GetValue() { return mainNumber; }
 
-        public override string ToString()
+
+        /*
+         * Function Overloading:
+         * אם עושים שתי פונקציות עם אותו השם אשר מקבלות פרמטרים שונים -> זה נקרא כך.
+         * המשתמש בקוד יוכל לקרוא לפונקציות עם איזה מסוגי הפרמטרים שירצה והוא יגיע למקום הנכון
+         */
+        public static int SumLetters(double number)
         {
-            double number = MainNumber;
-            int factorCounter = 0;
-            StringBuilder numInWords = new StringBuilder();
-
-            while (number != 0)
-            {
-                if (number % 1000 != 0)
-                {
-                    numInWords.Insert(0, readNumber(Convert.ToInt32(number % 1000)) + factor[factorCounter] + " ");
-                }
-                number -= number % 1000;
-                number /= 1000;
-                factorCounter++;
-            }
-            return numInWords.ToString();
+            NumericalExpression tmp = new NumericalExpression(number);
+            return SumLetters(tmp);
         }
+        public static int SumLetters(NumericalExpression numericalExpression)
+        {
+            string inWords = numericalExpression.ToString();
+            string[] words = inWords.Split(null);
+            int counter = 0;
+            foreach (var word in words)
+            {
+                counter += word.Length;
+            }
+            return counter;
+        }
+      
+        
 
     }
 }
--- 3/targil/targil/Program.cs
+++ 3/targil/targil/Program.cs
@@ -8,23 +8,24 @@ namespace targil
         {
             NumericalExpression num = new NumericalExpression(123456789159);
             Console.WriteLine(num.ToString());
-            /*LinkedList list = new LinkedList(3);
-            list.Append(1);
-            list.Append(4);
-            list.Append(2);
-            foreach (var nodeValue in list)
-            {
-                Console.Write(nodeValue + ", ");
-            }
-            Console.WriteLine();
-            list.sort();
-            foreach (var nodeValue in list)
-            {
-                Console.Write(nodeValue + ", ");
-            }
-            Console.WriteLine();
-            Console.WriteLine(list.GetMaxNode().Value);
-            Console.WriteLine(list.GetMinNode().Value);*/
+            Console.WriteLine(num.SumLetters());
+            //LinkedList list = new LinkedList(3);
+            //list.Append(1);
+            //list.Append(4);
+            //list.Append(2);
+            //foreach (var nodeValue in list)
+            //{
+            //    Console.Write(nodeValue + ", ");
+            //}
+            //Console.WriteLine();
+            //list.sort();
+            //foreach (var nodeValue in list)
+            //{
+            //    Console.Write(nodeValue + ", ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(list.GetMaxNode().Value);
+            //Console.WriteLine(list.GetMinNode().Value);
 
         }
     }
